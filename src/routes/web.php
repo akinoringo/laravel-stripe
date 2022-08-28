1856c2ad44fdad05a1d579f65296e3017133a06d
akinoringo <wbraver@outlook.jp>
2022-08-28 18:46:23 +0900

    feat: subscriptionリンクの追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/subscription', function () {
+    return view('subscription');
+})->middleware(['auth'])->name('subscription');
+
 require __DIR__.'/auth.php';
